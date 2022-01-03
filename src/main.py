7c569599e1665b6139c4760734529be7fe366581
zuzozuo <duraledsedlex.xx@gmail.com>
2022-01-04 00:13:27 +0100

    AH YES, BINARY SYSTEM KILLED ME. TIME TO CLEAN UP

--- src/main.py
+++ src/main.py
@@ -6,17 +6,17 @@ from pygame.locals import *
 
 test = Maze(MAZE_COLS, MAZE_ROWS, 0)
 test.generate()
-#test.solve_maze()
+test.find_path()
 print("---------------------------------------------")
 cells = test.get_cells()
-test.display_cells()
+#test.display_cells()
 #test.display_maze()
 
 #--------------------------------------------------
 
 pygame.init()  
 surface = pygame.display.set_mode((WINDOW_WIDTH + 3, WINDOW_HEIGHT + 10))
-font1 = pygame.font.SysFont(FONT_NAME, 20)
+font1 = pygame.font.SysFont(FONT_NAME, 10)
 
 running = True
 
@@ -32,12 +32,12 @@ while running:
         x = cells[i].x
         y = cells[i].y
 
-        if cells[i].is_start or cells[i].is_goal:
+        if (cells[i].walls & CELL_IS_START) | (cells[i].walls & CELL_IS_GOAL):
             pygame.draw.rect(surface, C_ENDPOINT , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
-        elif cells[i].walls & CELL_VISITED:
-            pygame.draw.rect(surface, C_VISITED , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
         elif cells[i].walls & CELL_BFS_PATH:
             pygame.draw.rect(surface, C_VISITED_2 , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
+        elif cells[i].walls & CELL_VISITED:
+            pygame.draw.rect(surface, C_VISITED , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
         else:
             pygame.draw.rect(surface, C_NORMAL , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT))
 
@@ -58,6 +58,6 @@ while running:
         if(cells[i].walls & WALL_LEFT):
             pygame.draw.line(surface, C_WHITE, (x * CELL_WIDTH , y * CELL_HEIGHT),                   (x * CELL_WIDTH , (y  * CELL_HEIGHT) + CELL_HEIGHT) , BORDER_THICKNESS)
         
-        surface.blit(img, (x * CELL_WIDTH + 10 , y * CELL_WIDTH + 10))
+        surface.blit(img, (x * CELL_WIDTH + 5 , y * CELL_WIDTH + 5))
 
     pygame.display.flip()
--- src/maze.py
+++ src/maze.py
@@ -1,7 +1,6 @@
 import random
 from consts import *
-import pygame
-
+from collections import deque
 
 class Cell():
     def __init__(self, width, height, id, x, y):
@@ -11,6 +10,7 @@ class Cell():
         self.walls = WALL_TOP + WALL_RIGHT + WALL_BOTTOM + WALL_LEFT + CELL_NOT_VISITED
         self.x = x  #x = id - (id // width) * width  np y = 93 - (93 /10 ) * 10 = 3 X -> COL NUMBER
         self.y = y  #y = id // maze_width - np x = 9 / 10 = 0  Y-> ROW NUMBER
+        self.bfs_visited = False
 
 
 class Maze():
@@ -21,10 +21,34 @@ class Maze():
         self.stack = [self.cells[start]]
         self.finished_maze = []
         
-
+        
+#------------- DISPLAY METHODS
     def display_cells(self):
         for cell in self.cells:
             print("id: " + str(cell.id) + " x: " + str(cell.x) + " y: " + str(cell.y) + " wall_value: " + str(bin(cell.walls)))
+
+#--------------TO_EDIT
+    def display_maze(self):
+
+        current = self.cells[0].id
+
+        for y in range(self.cols):
+            row = [''] * 2
+
+            for x in range(self.rows):
+                cell = self.cells[current + x]
+                row[0] += '00' if (cell.walls & WALL_TOP) else '01'
+                row[1] += '01' if (cell.walls & WALL_LEFT) else '11'
+
+            current += self.rows
+            [self.finished_maze.append((str + '0')) for str in row]
+
+        self.finished_maze.append('0' * (self.rows * 2 + 1))
+
+        for line in self.finished_maze:
+            print(line)
+
+#--------------- REVERSE BACKTRACKING MAZE GENERATION
     
     def check_neighbours(self, current):
         neighbours = []
@@ -48,7 +72,6 @@ class Maze():
         
         return neighbours
         
-
 
     def generate(self):
         while(len(self.stack) > 0):
@@ -92,48 +115,118 @@ class Maze():
                 self.stack.append(visit)    
 
 
-#--------------------
+#-----------------------HELPERS
     def reset_visit_flag(self):
         for i in range(0, len(self.cells)):
             self.cells[i].walls &= ~CELL_VISITED; 
 
     def get_cells(self):
         return self.cells
-#--------------TO_EDIT
-    def display_maze(self):
-
-        current = self.cells[0].id
-
-        for y in range(self.cols):
-            row = [''] * 2
-
-            for x in range(self.rows):
-                cell = self.cells[current + x]
-                row[0] += '00' if (cell.walls & WALL_TOP) else '01'
-                row[1] += '01' if (cell.walls & WALL_LEFT) else '11'
-
-            current += self.rows
-            [self.finished_maze.append((str + '0')) for str in row]
-
-        self.finished_maze.append('0' * (self.rows * 2 + 1))
-
-        for line in self.finished_maze:
-            print(line)
 
 
+#--------------------BFS SHORTEST PATH SEARCH 
+    def randomize_endpoints(self): #selects start and endpoint randomly
+        #start cell index
+            s_c = 0 
+        #goal cell index
+            g_c = random.randint(int((len(self.cells)/4) * 3), len(self.cells)-1)
+
+            self.cells[s_c].walls |= CELL_IS_START
+            self.cells[g_c].walls |= CELL_IS_GOAL
+
+            return(s_c, g_c)
 
 
-#----------------------------------
-def randomize_endpoints(self): #selects start and endpoint randomly
-    #start cell index
-        s_c = 0 
-    #goal cell index
-        g_c = random.randint(int((len(self.cells)/4) * 3), len(self.cells)-1)
+    def find_bfs_neighbours(self, current):
+        x = current.x
+        y = current.y
+        neighbours = []
 
-        self.cells[s_c].walls |= CELL_IS_START
-        self.cells[g_c].walls |= CELL_IS_GOAL
+        # has_t_w = current.walls & WALL_TOP
+        # has_b_w = self.cells[current.id - self.rows].walls & WALL_BOTTOM
+        # print(str(bin(current.walls)))
+        # print(str(bin(self.cells[current.id - self.rows].walls)))
+        # print(str(bin(has_t_w)))
+        # print(str(bin(has_b_w)))
+        # print(str(bin(has_t_w & has_b_w)))
+        # print(str(bin(~(has_t_w & has_b_w))))
+        #TOP
+        if ( y > 0 ) and not((self.cells[current.id - self.rows].walls & WALL_BOTTOM) or (current.walls & WALL_TOP)):
+            neighbours.append(self.cells[current.id  - self.rows ])
+            
+        #RIGHT
+        if ( x < self.rows -1 ) and not((self.cells[current.id + 1].walls & WALL_LEFT) or (current.walls & WALL_RIGHT)):
+            neighbours.append(self.cells[current.id + 1 ])
 
-        return(s_c, g_c)
+        #BOTTOM
+        if ( y < (self.cols - 1) ) and not((self.cells[current.id + self.rows].walls & WALL_TOP) or (current.walls & WALL_BOTTOM)):
+            neighbours.append(self.cells[current.id  + self.rows ])
+
+        #LEFT
+        if ( x > 0 ) and not((self.cells[current.id - 1].walls & WALL_RIGHT) or (current.walls & WALL_LEFT)):
+            neighbours.append(self.cells[current.id - 1 ])
+
+        return neighbours
+
+    def find_path(self):
+        s_c, g_c = self.randomize_endpoints()
+        start = self.cells[s_c]
+        goal = self.cells[g_c]
+        queue = deque()
+        searched = []
+        backtrace = {}
+#try to do it on ids and then generate proper flags maybe?
+
+        queue.append(start.id)
+
+        while(queue):
+            current = queue.popleft() #FIFO
+
+            if(current== goal.id):
+                break
+
+            neighbours = self.find_bfs_neighbours(self.cells[current])
+
+            for i in range(0, len(neighbours)):
+                check = neighbours[i].id
+                
+                if(not check in searched):
+                    searched.append(check)
+                    backtrace[check] = current
+                    queue.append(check)
+
+        
+
+        
+            print("Current: " + str(current))
+            print("Neighbours: " + str([neighbour.id for neighbour in neighbours]))
+        
+        
+        if(current == goal.id):
+            print(backtrace)
+            path = [goal.id]
+            current = goal.id
+
+            while(current != start.id):
+                current = backtrace[current]
+                path.insert(0, current)
+
+            for i in path:
+                self.cells[i].walls |= CELL_BFS_PATH
+                print(str(bin(self.cells[i].walls & CELL_BFS_PATH)))
+            
+            print(path)
+            
+
+            print("Woohoe!")
+            
+        else:
+            print("Brak trasy")
+        
+
+
+
+
 
 
 
