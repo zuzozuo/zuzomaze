70a2ba4c37e51cc2611bb7fb995310015ecd88da
zuzozuo <duraledsedlex.xx@gmail.com>
2022-01-05 00:39:23 +0100

    gitignore added

---
+++ src/cell.py
@@ -0,0 +1,16 @@
+from consts import W_TOP, W_RIGHT, W_BOTTOM, W_LEFT, CELL_NOT_VISITED
+
+
+class Cell():
+    def __init__(self, size, id, x, y):
+        self.size = size  # size for drawing imgs
+        self.id = id
+        self.walls = W_TOP | W_RIGHT | W_BOTTOM | W_LEFT | CELL_NOT_VISITED
+
+    # x = id - (id // width) * width : y = 93 - (93 /10 ) * 10 = 3 X -> COL NUM
+        self.x = x
+    # y = id // maze_width : x = 9 / 10 = 0  Y-> ROW NUM
+        self.y = y
+        self.bfs_step_num = "x"
+
+# EoF
--- src/main.py
+++ src/main.py
@@ -1,95 +1,78 @@
-from consts import *
+from consts import (MAZE_WIDTH, MAZE_HEIGHT, CELL_SIZE, IMG_MARGIN, FONT_SIZE,
+                    CELL_GOAL, CELL_START, CELL_PATH, C_NORMAL, C_ENDPOINT,
+                    C_BLACK, C_VISITED_2, W_TOP, W_RIGHT, W_BOTTOM, W_LEFT)
 from maze import Maze
-import pygame
-from pygame.locals import *
-from PIL import Image, ImageDraw
-
-#--------------------------------------------------
+from PIL import Image, ImageDraw, ImageFont
+import random, string
 
 
-def pygame_draw(cells):
-
-    pygame.init()  
-    surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    font1 = pygame.font.SysFont(FONT_NAME, 10)
-
-    running = True
-
-    while running: 
-        
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-
-        
-        for i in range(0, len(cells)):
-            img = font1.render(str(cells[i].id), True, C_BLACK)
-            x = cells[i].x
-            y = cells[i].y
-
-            if (cells[i].walls & CELL_IS_START) | (cells[i].walls & CELL_IS_GOAL):
-                pygame.draw.rect(surface, C_ENDPOINT , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
-            elif cells[i].walls & CELL_BFS_PATH:
-                pygame.draw.rect(surface, C_VISITED_2 , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
-            elif cells[i].walls & CELL_VISITED:
-                pygame.draw.rect(surface, C_VISITED , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT)) 
-            else:
-                pygame.draw.rect(surface, C_NORMAL , pygame.Rect(x *CELL_WIDTH, y *CELL_HEIGHT , CELL_WIDTH, CELL_HEIGHT))
-
-
-        #top line
-            if(cells[i].walls & WALL_TOP):
-                pygame.draw.line(surface, C_WHITE, (x * CELL_WIDTH, y * CELL_WIDTH),                     ((x * CELL_WIDTH) + CELL_WIDTH, y * CELL_HEIGHT), BORDER_THICKNESS)
-
-        #right line
-            if(cells[i].walls & WALL_RIGHT):
-                pygame.draw.line(surface, C_WHITE, ((x * CELL_WIDTH) + CELL_WIDTH  , y * CELL_HEIGHT),   ((x * CELL_WIDTH) + CELL_WIDTH , (y * CELL_HEIGHT) + CELL_HEIGHT), BORDER_THICKNESS)
-
-        #bottom line
-            if(cells[i].walls & WALL_BOTTOM):
-                pygame.draw.line(surface, C_WHITE, (x * CELL_WIDTH , (y * CELL_HEIGHT) + CELL_HEIGHT),                   ((x * CELL_WIDTH) + CELL_WIDTH,  (y * CELL_HEIGHT) + CELL_HEIGHT),  BORDER_THICKNESS)
-
-        #left line
-            if(cells[i].walls & WALL_LEFT):
-                pygame.draw.line(surface, C_WHITE, (x * CELL_WIDTH , y * CELL_HEIGHT),                   (x * CELL_WIDTH , (y  * CELL_HEIGHT) + CELL_HEIGHT) , BORDER_THICKNESS)
-            
-            surface.blit(img, (x * CELL_WIDTH + 5 , y * CELL_WIDTH + 5))
-
-        pygame.display.flip()
-
-#-------------------------------------------------------
-#----TO EDIT
-def draw_image(cells , cols, rows):
-    size_x = cols * CELL_BOX_SIZE + IMG_MARGIN * 2
-    size_y = rows * CELL_BOX_SIZE + IMG_MARGIN * 2
+def draw_image(cells, rows, cols, name):
+    size_x = rows * CELL_SIZE + IMG_MARGIN * 2  # ROWS - DRAWING FROM L TO R
+    size_y = cols * CELL_SIZE + IMG_MARGIN * 2  # COLS - DRAWING FROM U TO D
     img = Image.new('RGB', (size_x, size_y))
     draw = ImageDraw.Draw(img)
-
-
-    draw.rectangle([
-        (IMG_MARGIN, IMG_MARGIN), 
-        (IMG_MARGIN + cols * CELL_BOX_SIZE, IMG_MARGIN + rows * CELL_BOX_SIZE)],
-        fill=C_NORMAL, outline=C_WHITE)
+    txt_font = ImageFont.truetype("arial.ttf", FONT_SIZE)
 
     curr_pos = 0
-    x = IMG_MARGIN  #startposition
-    y = IMG_MARGIN  #startposition
+    x = IMG_MARGIN  # start
+    y = IMG_MARGIN  # start
 
-
-    for posy in range(0, rows):
-        for posx in range(0, cols):
+    for posy in range(0, cols):
+        for posx in range(0, rows):
             cell = cells[curr_pos + posx]
-            break
-        break
-    pass
+            x_txt = x + int(CELL_SIZE/3)
+            y_txt = y + int(CELL_SIZE/3)
+
+            if(cell.walls & CELL_START) | (cell.walls & CELL_GOAL):
+                draw.rectangle([(x, y), (x + CELL_SIZE, y + CELL_SIZE)], fill=C_ENDPOINT)
+                draw.text((x_txt, y_txt), str(cell.bfs_step_num), fill=C_BLACK, font=txt_font)
+
+            elif (cell.walls & CELL_PATH):
+                draw.rectangle([(x, y), (x + CELL_SIZE, y + CELL_SIZE)], fill=C_VISITED_2)
+
+                draw.text((x_txt, y_txt), str(cell.bfs_step_num), fill=C_BLACK, font=txt_font)
+            else:
+                draw.rectangle([(x, y), (x + CELL_SIZE, y + CELL_SIZE)], fill=C_NORMAL)
+
+            if (cell.walls & W_TOP):
+                draw.line([(x, y), (x + CELL_SIZE, y)])
+
+            if (cell.walls & W_RIGHT):
+                draw.line([(x + CELL_SIZE, y), (x + CELL_SIZE, y + CELL_SIZE)])
+
+            if (cell.walls & W_BOTTOM):
+                draw.line([(x, y + CELL_SIZE), (x + CELL_SIZE, y + CELL_SIZE)])
+
+            if (cell.walls & W_LEFT):
+                draw.line([(x, y), (x, y + CELL_SIZE)])
+
+            x += CELL_SIZE
+        curr_pos += rows
+        x = IMG_MARGIN
+        y += CELL_SIZE
+
+    img.show()
+    img.save(name + "_maze.png")
+
+# -------------------------------------------------------
+# ----------------- MAIN
+
+print("################################################")
+print("#                 W E L C O M E                #")
+print("#            I AM IMAGE MAZE GENERATOR         #")
+print("# I can generate, solve and draw maze for you! #")
+print("#            Project name: zuzomaze            #")
+print("#           Author (github): zuzozuo           #")
+print("#            It's aMAZEing  adventure!         #")
+print("################################################")
 
 
-#-------------------------------------------------------
-test = Maze(MAZE_COLS, MAZE_ROWS, 0)
-test.generate()
-test.find_path()
-print("---------------------------------------------")
-cells = test.get_cells()
-#pygame_draw(cells)
-#test.display_cells()
-#test.display_maze()
+aMAZEing = Maze(MAZE_WIDTH, MAZE_HEIGHT, 0)
+aMAZEing.generate()
+aMAZEing.find_path()
+cells = aMAZEing.get_cells()
+
+random_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 7))  
+draw_image(cells, MAZE_WIDTH, MAZE_HEIGHT, random_name)
+
+# EoF
